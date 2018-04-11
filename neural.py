0de3b46380a1c86c9338a9ed9c94e3c387e4a38f
bhavybarca <33397732+bhavybarca@users.noreply.github.com>
2018-04-11 15:28:27 +0530

    Create neural.py
    neural network with droupout

---
+++ neural.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import numpy as np
+from PIL import Image
+from operator import itemgetter
+
+
+path = "train1"
+folder_names = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+#print(folder_names)
+
+# Each row is an image
+img = np.zeros([94257, 2025], dtype = float)
+labels = np.zeros([94257])
+
+j = 0
+for i in range(10):
+	for image in os.listdir(path + "/" + folder_names[i]):
+		train_image = path + "/" + folder_names[i] + "/" + image
+		img[j] = np.asarray(Image.open(train_image).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		img[j] = img[j] / 255.0
+		labels[j] = folder_names[i]
+		j += 1
+y=labels
+X=img
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+from keras.layers import Dropout
+"""
+path = "train"
+folder_names = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+print(folder_names)
+
+# Each row is an image
+img = np.zeros([160, 2025], dtype = float)
+labels = np.zeros([160])
+
+j = 0
+for i in range(10):
+	for image in os.listdir(path + "/" + folder_names[i]):
+		train_image = path + "/" + folder_names[i] + "/" + image
+		img[j] = np.asarray(Image.open(train_image).convert('L').resize((45,45), Image.ANTIALIAS)).flatten()
+		img[j] = img[j] / 255.0
+		labels[j] = folder_names[i]
+		j += 1
+"""
+from sklearn.model_selection import train_test_split
+X, valData, y, valLabels = train_test_split(X, y, test_size=0.25, random_state=42)
+valData=img
+valLabels=labels
+y = np_utils.to_categorical(y)
+valLabels = np_utils.to_categorical(valLabels)
+num_classes = valLabels.shape[1]
+#print(num_classes)
+#550-6.70% , 1350-7.80%, 300-6.91% 500-6.46%
+
+for k in range(1):
+	model=Sequential()
+	model.add(Dense(500,input_dim=2025, kernel_initializer='normal', activation='relu'))
+	model.add(Dropout(0.14))
+	model.add(Dense(200, activation='relu'))
+	model.add(Dropout(0.14))
+	model.add(Dense(num_classes,kernel_initializer='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.fit(X, y, validation_data=(valData, valLabels), epochs=10, batch_size=200, verbose=2)
+	scores = model.evaluate(valData, valLabels, verbose=0)
+	print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+
